57d348c78f185fd3e901a812ef74c3ba1c750222
yan <yan@yan.com>
2022-04-16 02:17:45 +0900

    'fierFox3'

--- public/js/main.js
+++ public/js/main.js
@@ -27,7 +27,7 @@ $("#aj").click(function () {
 
 
 window.addEventListener('load', function(){
-      if(firefox && topPage){
+      if(firefox){
         $('#foxOnly').removeClass('displayNone');
         setTimeout(reloadTop, 1000*60*5, '');
       }
